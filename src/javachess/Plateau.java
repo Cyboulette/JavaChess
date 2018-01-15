bba471055f7171239f84fdf594abd611e111c293
ahertel <ahertel@RTAIW10-14-04.ly-sa.local>
2018-01-15 16:57:53 +0100

    Plateau

--- src/javachess/Plateau.java
+++ src/javachess/Plateau.java
@@ -5,10 +5,42 @@
  */
 package javachess;
 
+import java.util.ArrayList;
+import pieces.*;
+
 /**
  *
  * @author ahertel
  */
 public class Plateau {
+    
+    private ArrayList<Case> lesCases;
+    
+    public Plateau(){
+        lesCases = new ArrayList<Case>();
+    }
+    
+    public void initBoarder(){
+        Case uneCase; 
+        Tour uneT = new Tour();
+        Cavalier unC = new Cavalier();
+        Pion unP = new Pion();
+        Fou unF= new Fou();
+        Reine uneR = new Reine();
+        Roi unR = new Roi();
+        
+        for(int x=0; x < 8; x++){
+            for(int y =0; y < 8; y++){
+                uneCase = new Case(x, y);
+                lesCases.add(uneCase);
+                
+                if((x==0)&&(y==7||y==0))||((x==7)&&(y==7)){
+                
+                }
+            }
+        }
+    
+
+    }
     
 }
