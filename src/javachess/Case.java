02ba2c1e2169f9ed923909852b6b541eff6f257a
ahertel <ahertel@RTAIW10-14-04.ly-sa.local>
2018-01-15 16:58:15 +0100

    Plateau

---
+++ src/javachess/Case.java
@@ -0,0 +1,30 @@
+package javachess;
+
+import pieces.Piece;
+
+public class Case {
+
+    private int positionX;
+    private int positionY;
+    private Piece unePiece;
+    
+    public Case(int x, int y){
+        positionX = x;
+        positionY = y;
+        unePiece = null;
+        
+    }
+    
+    public boolean isEmpty(Piece uneP){
+        return this.unePiece == null;
+    }
+    
+    public Piece getUnePiece(){
+        return this.unePiece;
+    }
+
+    public void setUnePiece(Piece unePiece) {
+        this.unePiece = unePiece;
+    }
+    
+}
---
+++ src/pieces/Piece.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pieces;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author ahertel
+ */
+public abstract class Piece {
+    private int couleur;
+
+    
+    public abstract void seDeplacer();       
+    
+    public abstract void getDeplacement();
+
+    /**
+     * @param couleur the couleur to set
+     */
+    public void setCouleur(int couleur) {
+        this.couleur = couleur;
+    }
+        
+    
+}
