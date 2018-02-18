f5d4540227e5902fb7c3457b9e68057b671f9b6c
Cyboulette <cyboulette58@gmail.com>
2018-02-18 21:53:33 +0100

    LAtest version

---
+++ test/tests/JavaChessTestsSuite.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({tests.PlateauTest.class, tests.DeplacementsTest.class})
+public class JavaChessTestsSuite {
+    
+}
